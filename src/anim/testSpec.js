4a459e9f61b397d7182a2910fd4f57247074b978
Christian Lehner <lehner.chri@gmail.com>
2014-04-11 20:15:49 +0200

    added 'convertToOrientation'-function to return 'North'

--- src/anim/testSpec.js
+++ src/anim/testSpec.js
@@ -3,10 +3,17 @@ describe('get scroll offset', function() {
         expect(typeof getScrollOffset()).toEqual('number');
     });
 
-});
+    it('should return "North"', function() {
+        expect(convertToOrientation()).toEqual('North');
+    })
 
+});
 
 
 function getScrollOffset() {
     return 1;
+}
+
+function convertToOrientation() {
+    return 'North';
 }
