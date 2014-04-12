1d437503848cb29dc326fbd6bd6dea6c733eb62b
Christian Lehner <lehner.chri@gmail.com>
2014-04-12 08:16:55 +0200

    refactored tests

--- src/anim/testSpec.js
+++ src/anim/testSpec.js
@@ -3,6 +3,10 @@ describe('get scroll offset', function() {
     it('should return a number', function() {
         expect(typeof Controller.prototype.getScrollOffset()).toEqual('number');
     });
+});
+
+
+describe('convert angle to orientation', function() {
 
     it('should return "North"', function() {
         expect(Controller.prototype.convertToOrientation(0)).toEqual('North');
@@ -16,13 +20,18 @@ describe('get scroll offset', function() {
     it('should return "West"', function() {
         expect(Controller.prototype.convertToOrientation(270)).toEqual('West');
     });
+});
+
+describe('reset degrees after overflow (angle > 359 deg)', function() {
 
     it('should return 0 if Degree is over 359', function() {
         expect(Controller.prototype.resetDegrees(360)).toEqual(0);
     });
+
+    it('should return 0 if Degree is over 359', function() {
+        expect(Controller.prototype.resetDegrees(1000)).toEqual(280);
+    });
 });
-
-
 
 
 
@@ -60,8 +69,8 @@ Controller.prototype.convertToOrientation = function(angle) {
 
 Controller.prototype.resetDegrees = function(angle) {
     if(angle > 359) {
-        angle = angle - 360;
+        var counter = Math.floor(angle / 360);
+        angle = angle - (360 * counter);
     }
     return angle;
-    console.log(angle);
 }
